12d3c9bcf36598a8b83b78bc7b81fc3262877b5b
Vincent <vincent.diao2012@gmail.com>
2026-09-20 15:57:53 -0700

    Added Blablabla and other stuff for test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Configs/Globals/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Configs/Globals/Robot.java
@@ -48,7 +48,7 @@ public class Robot {
         windmillServo = hw.get(CRServo.class, "windmillServo");
         leftIntakeServo = hw.get(CRServo.class, "left_intake_servo");
         rightIntakeServo = hw.get(CRServo.class, "right_intake_servo");
-        //blablablabla
+        //blablablablablasblablablalb
 
         intake.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
